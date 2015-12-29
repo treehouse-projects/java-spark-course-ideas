0925e013e60614f6daef2d0ef93955ca015f9f23
Craig Dennis <craig@teamtreehouse.com>
2015-12-29 14:12:29 -0800

    Adds Handlebars templating and new index route

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -1,11 +1,15 @@
 package com.teamtreehouse.courses;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
 
-        get("/", (req, res) -> "Welcome Students!");
+        get("/", (req, res) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
