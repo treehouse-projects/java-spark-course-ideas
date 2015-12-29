cfebc7fda7880ebbbf5e688fa6f2bf593342c7f8
Craig Dennis <craig@teamtreehouse.com>
2015-12-29 15:18:11 -0800

    Adds sign-in flow

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -3,13 +3,23 @@ package com.teamtreehouse.courses;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Main {
     public static void main(String[] args) {
 
         get("/", (req, res) -> {
             return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.queryParams("username"));
+            return new ModelAndView(model, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
