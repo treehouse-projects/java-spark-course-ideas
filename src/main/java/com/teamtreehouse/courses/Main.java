1734a3a11adba30597dd084c7ab5e2729cf987de
Craig Dennis <craig@teamtreehouse.com>
2015-12-30 14:42:07 -0800

    Adds exception and flash messaging

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -1,10 +1,11 @@
 package com.teamtreehouse.courses;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import com.teamtreehouse.courses.model.CourseIdea;
 import com.teamtreehouse.courses.model.CourseIdeaDAO;
+import com.teamtreehouse.courses.model.NotFoundException;
 import com.teamtreehouse.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
@@ -13,6 +14,8 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class Main {
+    private static final String FLASH_MESSAGE_KEY = "flash";
+
     public static void main(String[] args) {
         staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
@@ -24,8 +27,8 @@ public class Main {
         });
 
         before("/ideas", (req, res) -> {
-            // TODO:csd - Send message about redirect...somehow.
             if (req.attribute("username") == null) {
+                setFlashMessage(req, "Whoops please log in first!");
                res.redirect("/");
                halt();
             }
@@ -33,6 +36,7 @@ public class Main {
 
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
+            model.put("flashMessage", captureFlashMessage(req));
             model.put("username", req.attribute("username"));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
@@ -47,6 +51,7 @@ public class Main {
 
         get("/ideas", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
+            model.put("flashMessage", captureFlashMessage(req));
             model.put("ideas", dao.findAll());
             return new ModelAndView(model, "ideas.hbs");
         }, new HandlebarsTemplateEngine());
@@ -68,9 +73,44 @@ public class Main {
 
         post("/ideas/:slug/vote", (req, res) -> {
             CourseIdea idea = dao.findBySlug(req.params("slug"));
-            idea.addVoter(req.attribute("username"));
+            boolean wasAdded = idea.addVoter(req.attribute("username"));
+            if (wasAdded) {
+                setFlashMessage(req, "Thanks for your vote!");
+            } else {
+                setFlashMessage(req, "Whoops you already voted!!!");
+            }
             res.redirect("/ideas");
             return null;
         });
+
+        exception(NotFoundException.class, (exc, req, res) -> {
+            res.status(404);
+            HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+            String html = engine.render(
+                    new ModelAndView(null, "not-found.hbs"));
+            res.body(html);
+        });
+    }
+
+    private static void setFlashMessage(Request req, String msg) {
+        req.session().attribute(FLASH_MESSAGE_KEY, msg);
+    }
+
+    private static String getFlashMessage(Request req) {
+        if (req.session(false) == null) {
+            return null;
+        }
+        if (!req.session().attributes().contains(FLASH_MESSAGE_KEY)) {
+            return null;
+        }
+        return (String) req.session().attribute(FLASH_MESSAGE_KEY);
+    }
+
+    private static String captureFlashMessage(Request req) {
+        String message = getFlashMessage(req);
+        if (message != null) {
+            req.session().removeAttribute(FLASH_MESSAGE_KEY);
+        }
+        return message;
     }
 }
